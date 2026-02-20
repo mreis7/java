7f0b119d8324fc227176a7f16d677585a2cbdc88
Matheus Reis <152439030+mreis7@users.noreply.github.com>
2026-02-20 11:53:42 -0300

    Delete java-fundamentos-console directory

--- java-fundamentos-console/OlaMundo.java
+++
@@ -1,8 +0,0 @@
-// Nomes de classes em java, sempre seguem o padrão de ter a primeira letra maiúscula, e se tiver palavras compostas, todas as primeiras letras terão que ser maiúsculas.
-// Exemplo: OlaMundo 
-
-public class OlaMundo {                                                     // classe
-	public static void main (String[] args) {                               // método
-		System.out.println("Olá Mundo!");
-	}
-}
